9f47b80469681212de312c67f65b1dba8dc98d72
ozgeozgec <89706915+ozgecinarci@users.noreply.github.com>
2023-03-07 14:02:53 +0300

    WebApiCore

---
+++ WebApiCore/Controllers/DesignationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiCore.Model;
+
+
+namespace WebApiCoreLecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignationController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+        public DesignationController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        // GET: api/lDesignation
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblDesignation>>> GetTblDesignation()
+        {
+            return await _context.TblDesignation.ToListAsync();
+        }
+    }
+}
